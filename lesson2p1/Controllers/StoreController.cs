3fb30c5629544e0c59be513b319abf3cedcf5538
karan singh <singhatwalkaran@gmail.com>
2018-10-11 04:18:17 -0400

    end lesson2p1  strongely typed album data

--- lesson2p1/Controllers/StoreController.cs
+++ lesson2p1/Controllers/StoreController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+// raference model name space
+
+using lesson2p1.Models;
+
 namespace lesson2p1.Controllers
 {
     public class StoreController : Controller
@@ -19,5 +23,18 @@ namespace lesson2p1.Controllers
             ViewBag.ProductName = ProductName;
             return View();
         }
+        // get store albums
+        public ActionResult Albums()
+        {
+            // mock up some album data
+            var albums = new List<Album>();
+
+            for (int i = 1; i <=10; i++)
+            {
+                albums.Add(new Album { Title = "Album" + i.ToString() });
+            }
+            return View(albums);
+        }
+
     }
 }
---
+++ lesson2p1/Models/Album.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lesson2p1.Models
+{
+    public class Album
+    {
+       public string Title { get; set; }
+    }
+}
