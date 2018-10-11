829c023b53f3cba4e7e26dbbd8efeebd31b66c4a
karan singh <singhatwalkaran@gmail.com>
2018-10-10 21:11:35 -0400

    lesson2. app set up

---
+++ lesson2p1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lesson2p1.MvcApplication" Language="C#" %>
---
+++ lesson2p1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lesson2p1.Startup))]
+namespace lesson2p1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
